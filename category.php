083e5998b5d2563f38e440813113523aed473276
Orlando M <jomy97@hotmail.com>
2018-05-21 07:26:38 -0500

    Modificación para la presentación del listado de las entradas en un artículo.

--- category.php
+++ category.php
@@ -56,30 +56,32 @@
 
      <div class="container">
        <div class="row">
-         <div class="col-xs-12 col-sm-9 ">
+         <div class="col-sm-12 col-md-9">
 
 					 <h1><?php single_cat_title(); ?></h1>
 
 					 <?php if ( have_posts() ) : ?>
-					   <section>
-					     <?php while ( have_posts() ) : the_post(); ?>
-					       <article>
-					         <header>
-					           <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-					           <time datatime="<?php the_time('Y-m-j'); ?>"><?php the_time('j F, Y'); ?></time>
-					 					<?php the_category (); ?>
-					         </header>
-					         <?php the_excerpt(); ?>
-					         <footer>
-					             <address>Por <?php the_author_posts_link() ?></address>
-					         </footer>
-					       </article>
-					     <?php endwhile; ?>
-					     <div class="pagination">
-					       <span class="in-left"><?php next_posts_link('« Entradas antiguas'); ?></span>
-					       <span class="in-right"><?php previous_posts_link('Entradas más recientes »'); ?></span>
-					     </div>
-					   </section>
+             <section>
+  				     <?php while ( have_posts() ) : the_post(); ?>
+                   <div class="col-sm-12 col-md-4">
+    					       <article>
+    					         <header>
+    					           <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    					           <time datatime="<?php the_time('Y-m-j'); ?>"><?php the_time('j F, Y'); ?></time>
+    					 					<?php the_category (); ?>
+    					         </header>
+    					         <?php the_excerpt(); ?>
+    					         <footer>
+    					             <address>Por <?php the_author_posts_link() ?></address>
+    					         </footer>
+    					       </article>
+                   </div>
+  				     <?php endwhile; ?>
+  				     <div class="pagination">
+  				       <span class="in-left"><?php next_posts_link('« Entradas antiguas'); ?></span>
+  				       <span class="in-right"><?php previous_posts_link('Entradas más recientes »'); ?></span>
+  				     </div>
+  				   </section>
 					 <?php else : ?>
 					   <p><?php _e('Ups!, no hay entradas.'); ?></p>
 					 <?php endif; ?>
